9d2754559005eb2f51b178f81fbdfcf4eb926b60
melodytempleton <mtempletoncantu@gmail.com>
2017-05-20 17:12:17 -0500

    Complete counter project

--- src/Counter.java
+++ src/Counter.java
@@ -3,5 +3,26 @@
  */
 public class Counter {
 
+    private String name= null;
+    private int currentNumber = 0;
+
+    public Counter(String str){
+        this.name=str;
+    }
+
+    public int increment(){
+
+        return currentNumber++;
+
+    }
+
+   public int getCurrentValue(){
+
+        return currentNumber;
+    }
+
+    public String myString(){
+        return name +": " + currentNumber;
+    }
 
 }
